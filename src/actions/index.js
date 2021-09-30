e1f083776a29a6ae40a62a4e8b74413378256cbc
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-09-30 17:54:15 +0530

    async redux

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceholder from "../Apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
