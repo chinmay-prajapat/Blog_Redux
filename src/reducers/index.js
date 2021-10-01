4c61c045b14b5b6e3baa250e4bf2f4c1c74cc5bb
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-01 10:50:14 +0530

    display data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReducers from "./usersReducers";
 export default combineReducers({
   posts: postsReducer,
+  users: usersReducers,
 });
