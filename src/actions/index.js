550fcf5f8a5e0f5a5411ae1f29f563d3a4cac8d7
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-01 10:39:37 +0530

    display data

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,7 @@ export const fetchPosts = () => async (dispatch) => {
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+export const fetchUser = (id) => async (dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: res.data });
+};
---
+++ src/components/UserHeader.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class UserHeader extends Component {
+  render() {
+    return <div>User Header</div>;
+  }
+}
+export default UserHeader;
